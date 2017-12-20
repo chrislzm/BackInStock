62de44a82ef0ac79bc1da242c74ebb5564b692e6
chrislzm <me@chrisleung.com>
2017-12-20 00:04:46 -0800

    feat: Added security for getting/updating/deleting data

---
+++ RestApi/src/main/java/com/spring/restapi/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.spring.restapi.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        	.csrf().disable()
+        	.authorizeRequests()
+        		.antMatchers(HttpMethod.GET, "/**").authenticated()
+        		.antMatchers(HttpMethod.PUT, "/**").authenticated()
+        		.antMatchers(HttpMethod.DELETE, "**").authenticated()
+        		.anyRequest().permitAll()
+        		.and()
+    		.httpBasic().and()
+    		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
