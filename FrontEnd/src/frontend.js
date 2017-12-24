65fe90162678f91a0d40f8dd330df8daf191d704
chrislzm <me@chrisleung.com>
2017-12-23 22:54:48 -0800

    feat: Added sku and constants to identify form/fields

--- FrontEnd/src/frontend.js
+++ FrontEnd/src/frontend.js
@@ -1,14 +1,26 @@
+const NOTIFICATION_FORM_ID = "stock-notification";
+const NOTIFICATION_FORM_SKU_ID = "sku";
+const INPUT_SKU_ID = "product-selector";
+
 function onSubmit( form ){
-  var data = getFormData($("#stock-notification"));
+  var data = getFormData($(`#${NOTIFICATION_FORM_ID}`));
   console.log(data);
   return false;
 }
 
 function getFormData($form){
+  // Copy required data into hidden form fields
+  copyInputValue(INPUT_SKU_ID,NOTIFICATION_FORM_SKU_ID);
+
+  // Create JSON Object
   var unindexed_array = $form.serializeArray();
   var indexed_array = {};
   $.map(unindexed_array, function(n, i){
       indexed_array[n['name']] = n['value'];
   });
   return indexed_array;
+}
+
+function copyInputValue(id1,id2){
+  document.getElementById(id2).value = document.getElementById(id1).value;
 }
