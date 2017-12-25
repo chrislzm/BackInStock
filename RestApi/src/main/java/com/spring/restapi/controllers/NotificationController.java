4a84fbd59743c801764decdd4c8f610a68bc0533
chrislzm <me@chrisleung.com>
2017-12-24 21:37:09 -0800

    feat: Enabled CORS

--- RestApi/src/main/java/com/spring/restapi/controllers/NotificationController.java
+++ RestApi/src/main/java/com/spring/restapi/controllers/NotificationController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Chris Leung
  *
  */
+@CrossOrigin
 @RestController
 public class NotificationController {
 
