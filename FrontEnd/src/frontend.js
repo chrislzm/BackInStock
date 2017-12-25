997cd2da1d463bff7932881d8e935b80b13e82c4
chrislzm <me@chrisleung.com>
2017-12-24 20:52:09 -0800

    feat: Added email address validation

--- FrontEnd/src/frontend.js
+++ FrontEnd/src/frontend.js
@@ -9,6 +9,7 @@ function onSubmit( form ){
 }
 
 function getFormData($form){
+
   // Copy required data into hidden form fields
   copyInputValue(INPUT_SKU_ID,NOTIFICATION_FORM_SKU_ID);
 
@@ -18,9 +19,16 @@ function getFormData($form){
   $.map(unindexed_array, function(n, i){
       indexed_array[n['name']] = n['value'];
   });
-  return indexed_array;
+  if(isValidEmail(indexed_array["email"])) return indexed_array;
+  else alert("Please enter a valid email address.");
 }
 
 function copyInputValue(id1,id2){
   document.getElementById(id2).value = document.getElementById(id1).value;
+}
+
+function isValidEmail(address)
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(address)) return true;
+ else return false;
 }
