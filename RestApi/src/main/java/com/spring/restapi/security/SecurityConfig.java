2ed372c252960f5398d4609c67a469d1b5049763
chrislzm <me@chrisleung.com>
2018-01-02 21:22:31 -0800

    refactor: Removed unused imports

--- RestApi/src/main/java/com/spring/restapi/security/SecurityConfig.java
+++ RestApi/src/main/java/com/spring/restapi/security/SecurityConfig.java
@@ -1,9 +1,7 @@
 package com.spring.restapi.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
