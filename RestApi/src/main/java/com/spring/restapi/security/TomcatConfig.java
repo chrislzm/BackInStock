2c5e9eecc5dc5ded81f0c1f787ada66f02f3e713
chrislzm <me@chrisleung.com>
2018-01-08 22:55:06 -0800

    feat: Server now accessible via HTTP and HTTPS

---
+++ RestApi/src/main/java/com/spring/restapi/security/TomcatConfig.java
@@ -0,0 +1,33 @@
+package com.spring.restapi.security;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+    
+    @Value("${server.http.port}")
+    private int httpPort;
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                if (container instanceof TomcatEmbeddedServletContainerFactory) {
+                    TomcatEmbeddedServletContainerFactory containerFactory =
+                            (TomcatEmbeddedServletContainerFactory) container;
+
+                    Connector connector = new Connector(TomcatEmbeddedServletContainerFactory.DEFAULT_PROTOCOL);
+                    connector.setPort(httpPort);
+                    containerFactory.addAdditionalTomcatConnectors(connector);
+                }
+            }
+        };
+    }
+}
