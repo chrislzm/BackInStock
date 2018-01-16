5e59517cae18501819af1e24f030a6f0d1609f7f
chrislzm <me@chrisleung.com>
2018-01-15 21:51:37 -0800

    feat: Implemented JSON object container

--- NotificationService/src/main/java/com/chrisleung/notifications/service/Notification.java
+++ NotificationService/src/main/java/com/chrisleung/notifications/service/Notification.java
@@ -1,5 +1,85 @@
+// TODO: This is essentially identical to Notification class on the REST API server; should create a single shared class
+
 package com.chrisleung.notifications.service;
 
-public class Notification {
+import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Notification {
+	   	String id;
+	    String email;
+	    String sku;
+	    @DateTimeFormat(iso = ISO.DATE_TIME)
+		Date createdDate;
+	    Boolean sent;
+	    @DateTimeFormat(iso = ISO.DATE_TIME)
+		Date sentDate;
+	    
+	    public Notification() {
+	    }
+	    
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public Date getCreatedDate() {
+			return createdDate;
+		}
+
+		public void setCreatedDate(Date createdDate) {
+			this.createdDate = createdDate;
+		}
+		
+		public String getSku() {
+			return sku;
+		}
+
+		public void setSku(String sku) {
+			this.sku = sku;
+		}
+
+		public Boolean getIsSent() {
+			return sent;
+		}
+
+		public void setIsSent(Boolean sent) {
+			this.sent = sent;
+		}
+
+		public Date getSentDate() {
+			return sentDate;
+		}
+
+		public void setSentDate(Date sentDate) {
+			this.sentDate = sentDate;
+		}
+		
+	    @Override
+	    public String toString() {
+	        return "Notification{" +
+	                "id='" + id + '\'' +
+	                ", email='" + email + '\'' +
+	                ", sku='" + sku + '\'' +
+	                ", createdDate='" + createdDate + '\'' +
+	                ", sent='" + sent + '\'' +
+	                ", sentDate='" + sentDate + '\'' +
+	                '}';
+	    }
 }
