016c839bd770b5505ea8d279109c525331c9b8f1
chrislzm <me@chrisleung.com>
2018-01-23 22:48:35 -0800

    Initial commit

---
+++ Objects/src/main/java/com/chrisleung/notifications/objects/NotificationWrapper.java
@@ -0,0 +1,40 @@
+package com.chrisleung.notifications.objects;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class NotificationWrapper {
+ 
+    @DateTimeFormat(iso = ISO.DATE_TIME)
+    private Date currentDate;
+    private Iterable<Notification> notifications;
+    
+    public NotificationWrapper() {
+    }
+    
+    public NotificationWrapper(Iterable<Notification> list, Date d) {
+        notifications = list;
+        this.currentDate = d;
+    }
+
+    public Date getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(Date currentDate) {
+        this.currentDate = currentDate;
+    }
+
+    public Iterable<Notification> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(Iterable<Notification> notifications) {
+        this.notifications = notifications;
+    }
+}
