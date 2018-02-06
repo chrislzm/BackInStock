bc17e67fbdf22b2dabb603c4ffecfa5cf67137ed
chrislzm <me@chrisleung.com>
2018-02-05 22:49:38 -0800

    fix: Removed ES6 syntax for backward compatibility

--- FrontEnd/src/stock-notification-api.js
+++ FrontEnd/src/stock-notification-api.js
@@ -1,13 +1,15 @@
-const SERVER_URL = 'http://service.artofsilk.com:8080'
+const SERVER_URL = 'http://localhost:8080'
 const HEADERS = {
   'Content-Type': 'application/json'
 }
 
-const submitNotification = (body) => (
-  fetch(`${SERVER_URL}/notifications`, {
+function submitNotification(body) {
+  return fetch(SERVER_URL} + "/notifications", {
     method: 'POST',
     headers: HEADERS,
     body:JSON.stringify(body)
    })
-  .then(res => res.json())
-)
+  .then(function(res) {
+    return res.json();
+  })
+}
