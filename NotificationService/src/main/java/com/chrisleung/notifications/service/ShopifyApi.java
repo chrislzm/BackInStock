a272808de3bddce708dd4911aad2c86328f15c38
chrislzm <me@chrisleung.com>
2018-02-18 16:13:54 -0800

    refactor: Now using updated interfaces

--- NotificationService/src/main/java/com/chrisleung/notifications/service/ShopifyApi.java
+++ NotificationService/src/main/java/com/chrisleung/notifications/service/ShopifyApi.java
@@ -1,5 +1,8 @@
 package com.chrisleung.notifications.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpMethod;
@@ -8,9 +11,9 @@ import org.springframework.http.client.support.BasicAuthorizationInterceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import com.shopify.api.ProductWrapper;
-import com.shopify.api.VariantWrapper;
-
+import com.chrisleung.notifications.objects.ProductVariant;
+import com.chrisleung.notifications.objects.StoreApi;
+import com.shopify.api.*;
 
 /**
  * Implements an interface to a Shopify store via its REST API. 
@@ -26,7 +29,8 @@ public class ShopifyApi implements StoreApi {
     private String urlPostfix;
     private BasicAuthorizationInterceptor auth; // For Username+Password auth
     private RestTemplate restTemplate;
-
+    private Map<Integer,ProductVariant> cache;
+    
     @Autowired
     ShopifyApi(ShopifyApiConfig config, RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
@@ -34,23 +38,32 @@ public class ShopifyApi implements StoreApi {
         this.variantUrl = config.getProduct().getVariant().getUrl();
         this.productUrl = config.getProduct().getUrl();
         this.urlPostfix = config.getUrlPostFix();
+        this.cache = new HashMap<>();
     }
     
-    @Override
-    public com.chrisleung.notifications.objects.Variant getVariant(int variantId) {
+    public Variant getVariant(int variantId) {
         restTemplate.getInterceptors().add(auth);
         String url = String.format("%s%s%s",variantUrl,variantId,urlPostfix);
         ResponseEntity<VariantWrapper> shopifyResponse = restTemplate.exchange(url, HttpMethod.GET, null, VariantWrapper.class);
         restTemplate.getInterceptors().remove(0);
-        return new ShopifyVariant(shopifyResponse.getBody().getVariant());
+        return shopifyResponse.getBody().getVariant();
     }
 
-    @Override
-    public com.chrisleung.notifications.objects.Product getProduct(int productId) {
+    public Product getProduct(int productId) {        
         restTemplate.getInterceptors().add(auth);
         String url = String.format("%s%s%s",productUrl,productId,urlPostfix);
         ResponseEntity<ProductWrapper> shopifyResponse = restTemplate.exchange(url, HttpMethod.GET, null, ProductWrapper.class);
         restTemplate.getInterceptors().remove(0);
-        return new ShopifyProduct(shopifyResponse.getBody().getProduct());
+        return shopifyResponse.getBody().getProduct();
+    }
+
+    @Override
+    public ProductVariant getProductVariant(int variantId) {
+        if(!cache.containsKey(variantId)) {
+            Variant v = getVariant(variantId);
+            Product p = getProduct(v.getProduct_id());
+            cache.put(variantId, new ShopifyProductVariant(p,v));
+        }
+        return cache.get(variantId);
     }
 }
