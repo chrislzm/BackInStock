a3e5f505db136720d9eed1409d84bbb34f8a96c5
chrislzm <me@chrisleung.com>
2018-02-18 16:14:09 -0800

    refactor: Using new ProductVariant interface

--- NotificationService/src/main/java/com/chrisleung/notifications/service/EmailNotification.java
+++ NotificationService/src/main/java/com/chrisleung/notifications/service/EmailNotification.java
@@ -10,38 +10,27 @@ import com.chrisleung.notifications.objects.*;
  * @author Chris Leung
  */
 class EmailNotification {
-    private Product product;
-    private Variant variant;
+    private ProductVariant productVariant;
     private Notification notification;
     
-    EmailNotification(Product p, Variant v, Notification n) {
-        this.product = p;
-        this.variant = v;
+    EmailNotification(ProductVariant pv, Notification n) {
+        this.setProductVariant(pv);
         this.notification = n;
     }
-    
-    public Product getProduct() {
-        return product;
+
+    public ProductVariant getProductVariant() {
+        return productVariant;
     }
-    public void setProduct(Product product) {
-        this.product = product;
+
+    public void setProductVariant(ProductVariant productVariant) {
+        this.productVariant = productVariant;
     }
-    public Variant getVariant() {
-        return variant;
-    }
-    public void setVariant(Variant variant) {
-        this.variant = variant;
-    }
+
     public Notification getNotification() {
         return notification;
     }
+    
     public void setNotification(Notification notification) {
         this.notification = notification;
-    }
-
-    @Override
-    public String toString() {
-        return "EmailNotification [product=" + product + ", variant=" + variant + ", notification=" + notification
-                + "]";
     }
 }
