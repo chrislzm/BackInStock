b8fd75c9cdd3a084ff560c750d853cb333a24ddb
chrislzm <me@chrisleung.com>
2018-03-01 15:45:28 -0800

    feat: Implemented get benchmark

--- Benchmark/src/main/java/com/chrisleung/notifications/tools/restapi/benchmark/GetNotificationJob.java
+++ Benchmark/src/main/java/com/chrisleung/notifications/tools/restapi/benchmark/GetNotificationJob.java
@@ -6,28 +6,30 @@ import java.util.Date;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import com.chrisleung.notifications.objects.Notification;
+import com.chrisleung.notifications.objects.NotificationWrapper;
 
 public class GetNotificationJob implements Runnable {
 
     RestTemplate restTemplate;
     HttpEntity<Notification> entity;
-    String endPoint;
-    ArrayList<Date> completedTimes;
+    String url;
+    ArrayList<Object[]> completedData;
     
-    GetNotificationJob(RestTemplate r, String url, HttpHeaders headers, int index, ArrayList<Date> c) {
+    GetNotificationJob(RestTemplate r, String endpoint, String id, HttpHeaders headers, ArrayList<Object[]> c) {
         restTemplate = r;
-        endPoint = url;
-        completedTimes = c;
+        url = endpoint + '/' + id;
+        completedData = c;
     }
     
     @Override
     public void run() {
-        restTemplate.exchange(endPoint, HttpMethod.POST, entity, Response.class);
-        synchronized(completedTimes) {
-            completedTimes.add(new Date());
+        ResponseEntity<NotificationWrapper> response = restTemplate.exchange(url, HttpMethod.GET, null, NotificationWrapper.class);
+        synchronized(completedData) {
+            completedData.add(new Object[] {new Date()});
         }
     }
 }
