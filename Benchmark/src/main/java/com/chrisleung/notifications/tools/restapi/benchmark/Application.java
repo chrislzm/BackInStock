aa4d6e958f486632072015f5c5413fbb61a1e0b5
chrislzm <me@chrisleung.com>
2018-02-28 22:36:58 -0800

    feat: Implemented multithreading/concurrent connections

--- Benchmark/src/main/java/com/chrisleung/notifications/tools/restapi/benchmark/Application.java
+++ Benchmark/src/main/java/com/chrisleung/notifications/tools/restapi/benchmark/Application.java
@@ -1,5 +1,10 @@
 package com.chrisleung.notifications.tools.restapi.benchmark;
 
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -7,14 +12,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-
-import com.chrisleung.notifications.objects.*;
 
 /**
  * This class implements a simple benchmarking tool for the Database REST API.
@@ -27,11 +27,17 @@ import com.chrisleung.notifications.objects.*;
 public class Application {
         
     @Value("${restapi.benchmark.endpoint}")
-    private String url;
-    
+    private String endpoint;
+    @Value("${restapi.benchmark.requests.total}")
+    private int numRequests;
+    @Value("${restapi.benchmark.requests.concurrent}")
+    private int numConcurrent;
+    @Value("${restapi.benchmark.request.email}")
+    private String email;
+
     @Autowired
     private RestTemplate restTemplate;
-    
+
 	public static void main(String args[]) {
 		SpringApplication.run(Application.class);
 	}
@@ -44,17 +50,26 @@ public class Application {
 	@Bean
 	public CommandLineRunner run() throws Exception {
 		return args -> {
-		    Notification obj = new Notification("christopher.leung@gmail.com",3);
-
-		    //set your headers
-		    HttpHeaders headers = new HttpHeaders();
-		    headers.setContentType(MediaType.APPLICATION_JSON);
-
-		    //set your entity to send
-		    HttpEntity<Notification> entity = new HttpEntity<>(obj,headers);
-
-		    ResponseEntity<Response> response = restTemplate.exchange(url, HttpMethod.POST, entity, Response.class);
-		    System.out.println("RESPONSE: " + response.getBody().getSaved());
+	        HttpHeaders headers = new HttpHeaders();
+	        headers.setContentType(MediaType.APPLICATION_JSON);
+	        PostNotificationJob[] jobs = new PostNotificationJob[numRequests];
+	        
+	        /* Create the jobs first */
+		    for(int i=0; i<numRequests; i++) {
+		        jobs[i] = new PostNotificationJob(restTemplate,endpoint,headers,email,i);
+		    }
+		    
+		    /* Submit jobs */
+		    Date start = new Date();
+            ExecutorService threadPool = Executors.newFixedThreadPool(numConcurrent);
+            for(PostNotificationJob job : jobs) {
+                threadPool.execute(job);
+            }
+            threadPool.shutdown();
+            threadPool.awaitTermination(60, TimeUnit.SECONDS);
+            Date end = new Date();
+            long elapsed = end.getTime()-start.getTime();
+            System.out.println(String.format("Completed %s requests with %s concurrent connections in %s seconds. Average = %s requests/second", numRequests,numConcurrent,elapsed/1000.0f,numRequests/(elapsed/1000.0f)));
 		};
 	}
 }
