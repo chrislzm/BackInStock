fa8a435094c0c7e649e1588a1a2af0bde77ba84f
chrislzm <me@chrisleung.com>
2018-03-02 13:31:05 -0800

    feat: Added method to clear cache

--- Objects/src/main/java/com/chrisleung/notifications/objects/StoreApi.java
+++ Objects/src/main/java/com/chrisleung/notifications/objects/StoreApi.java
@@ -7,4 +7,13 @@ package com.chrisleung.notifications.objects;
  */
 public interface StoreApi {
     public ProductVariant getProductVariant(long variantId);
+    
+    /**
+     * This method is used to clear a cache of Product/Variant data, if any.
+     * This is useful if you are caching the data in order to reduce API calls.
+     * If product and variant data is updated (e.g. inventory quantity) and we
+     * are using cached data, we need to clear the cached data between runs
+     * before we'll see those updates. 
+     */
+    public void clearCache();
 }
